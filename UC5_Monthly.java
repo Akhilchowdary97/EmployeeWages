6b4534c67c6d25114ce8885ebfa2ab9ac9c7d129
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-11 17:43:19 +0530

    Monthly Wages

---
+++ UC5_Monthly.java
@@ -0,0 +1,13 @@
+package EmpWage;
+
+public class UC5_MonthlyWages {
+	public static void main(String args[])
+    {
+        System.out.println("================================"+"\n"+"Enter Full Time Employee Details"+"\n"+"================================"+"\n");
+        Employees ob3 = new Employees();
+        ob3.monthwisepay();
+        ob3.display1();
+        
+    }
+
+}
