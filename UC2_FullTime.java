ec0c43fd29765ce1fc37148752839f5435769b1b
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-11 17:21:01 +0530

    DAily Employee Wage per Hour And Full Day

---
+++ UC2_FullTime.java
@@ -0,0 +1,14 @@
+package EmpWage;
+
+public class UC2_fullTimeEmployeedailywage 
+{ 
+	public static void main(String args[])
+	    {
+	        System.out.println("================================"+"\n"+"Enter Full Time Employee Details"+"\n"+"================================"+"\n");
+	        fullTimeEmployees ob1 = new fullTimeEmployees();
+	        ob1.calculatepay();
+	        ob1.display();
+	        
+	    }
+}
+
