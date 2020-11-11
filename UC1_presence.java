21b462ab350b4e5e82b5025f726fb7842b41b9d9
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-11 16:36:08 +0530

    Employee Present or abscent

---
+++ UC1_presence.java
@@ -0,0 +1,13 @@
+package EmpWage;
+
+public class UC1_presence {
+	public static void main(String[] args) {
+		//Constants
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");
+	}
+}
