7e0974deedb5f58d0686ab6aece235f2e5fb3c80
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-11 17:48:01 +0530

    Monthly and hours based Wages

---
+++ MonthlyWage.java
@@ -0,0 +1,13 @@
+package EmpWage;
+
+public class UC6_MonthlyWithHoursWages {
+	public static void main(String args[])
+    {
+        System.out.println("================================"+"\n"+"Enter Employee Details"+"\n"+"================================"+"\n");
+        Employees ob4 = new Employees();
+        ob4.monthwisepay1();
+        ob4.display2();
+        
+    }
+
+}
